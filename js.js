c877e2296b808be382a80aa881d89eae9e661a97
Rz0n <arestor16@mail.bg>
2024-10-25 22:38:24 +0300

    Create core logic for game

--- js.js
+++ js.js
@@ -1,40 +1,74 @@
-let humanScore = 0;
-let computerScore = 0;
+let min = 1;
+let max = 3;
+
+let play = 5;
+
+let randomNumber;
 
 
-function getHumanChoise() {
-    let humanChoises = prompt("What is you Choise?: ");
-    console.log(humanChoises);
+let player = prompt("Make your Choise");
+
+
+function computerChoise(choise) {
     
-    return humanChoises;
-}
+    let randomNumber = Math.floor(Math.random() * max) + min;
 
-getHumanChoise();
-
-function getComputerChoise() {
     
-    let randomComputerChoise = Math.random() * 3;
-
-    if (randomComputerChoise <= 1){
-        console.log("Rock");
-    } else if (randomComputerChoise <= 2) {
-        console.log('Scissors');
+    if (randomNumber == 1) {
+        choise = "Rock";
+    } else if (randomNumber == 2) {
+        choise = "Paper";
     } else {
-        console.log("Paper");
+        choise = "Scissors";
     }
 
-
-    return random;
+    return choise;
 }
 
-getComputerChoise();
 
-function playRound(humanChoise, computerChoise) {
-    humanChoise = getHumanChoise();
-    computerChoise = getComputerChoise();
+function playGround() {
 
-    if (humanChoise === "Rock" && computerChoise === "Scissors" ){
-        console.log("You win ");
+    if (player == "Rock" && computer == "Rock") {
+        console.log(player);
+        console.log(computer);
+        console.log(`${player} vs ${computer}: Is a Tie`);
+    } else if (player == "Rock" && computer == "Paper") {
+        console.log(player);
+        console.log(computer);
+        console.log(`${player} vs ${computer}: Rock lose Paper!!\nComputer Win!!`);
+    } else if (player == "Rock" && computer == "Scissors") {
+        console.log(player);
+        console.log(computer);
+        console.log(`${player} vs ${computer}: Rock beats Scissors!!\nPlayer Win!!`);
+    } else if (player == "Paper" && computer == "Rock") {
+        console.log(player);
+        console.log(computer);
+        console.log(`${player} vs ${computer}: Paper beats Rock!!\nPlayer Win!!`);
+    } else if (player == "Paper" && computer == "Paper") {
+        console.log(player);
+        console.log(computer);
+        console.log(`${player} vs ${computer}: Is a tie!!`);
+    } else if (player == "Paper" && computer == "Scissors") {
+        console.log(player);
+        console.log(computer);
+        console.log(`${player} vs ${computer}: Paper lose to Scissors!!\nComputer Win!!`);
+    } else if (player == "Scissors" && computer == "Rock") {
+        console.log(player);
+        console.log(computer);
+        console.log(`${player} vs ${computer}: Scissors lose to Rock!!\nComputer Win!!`);
+    } else if (player == "Scissors" && computer == "Paper") {
+        console.log(player);
+        console.log(computer);
+        console.log(`${player} vs ${computer}: Scissors beats Paper!!\nPlayer Win!!`);
+    } else if (player == "Scissors" && computer == "Scissors") {
+        console.log(player);
+        console.log(computer);
+        console.log(`${player} vs ${computer}: Is a tie!!`);
     }
+
 }
 
+let computer = computerChoise();
+
+
+playGround();
